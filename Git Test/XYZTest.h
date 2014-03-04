49702ecd1a6c08b66487e5291e385a231fe06811
Aniket Ghode <aniketghode@KnightDark.local>
2014-03-04 18:49:02 +0530

    Added test class

---
+++ Git Test/XYZTest.h
@@ -0,0 +1,13 @@
+//
+//  XYZTest.h
+//  Git Test
+//
+//  Created by Aniket Ghode on 3/4/14.
+//  Copyright (c) 2014 Silicus Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYZTest : NSObject
+
+@end
